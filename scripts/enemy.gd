69167c4a2a63d969e6dc8a160e396b9581334ade
Ryan Chan <ryan.e.chan@gmail.com>
2024-05-13 21:52:37 -0400

    added enemy fish sprite animation

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 @onready var player = get_node("/root/Game/Player")
+@onready var animated_sprite = $AnimatedSprite2D
 
 var SPEED = 1
+
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * SPEED
+	if direction.x > 0:
+		animated_sprite.flip_h = true
+	elif direction.x < 1:
+		animated_sprite.flip_h = false
 
 	move_and_slide()
