2edc7e06863bfacb59c21fbc42b1e916d8843f36
Kelvin Dela Cruz <kelvin.dcruz@gmail.com>
2024-05-14 20:31:54 -0400

    reworking weapon

--- scripts/player/attacks/bubble.gd
+++ scripts/player/attacks/bubble.gd
@@ -1,12 +1,34 @@
 extends Area2D
 
+var level = 1
+var hp = 1 # hp of projectile; how many enemies 1 projectile can hit
+var speed = 100
+var damage = 5
+var knock_amount = 100
+var attack_size = 1.0
+
+var target = Vector2.ZERO
+var angle = Vector2.ZERO
+
+@onready var player = get_tree().get_first_node_in_group("player")
+
 var offset = randf_range(-0.2,0.2) # adds spray effect to the bubble
-var speed = 250 # initial bubble speed
-var travel_distance = 0 # destroy bubble after it's travelled enough
+
+func _ready():
+	angle = global_position.direction_to(target)
+	rotation = angle.angle() + deg_to_rad(135)
+	match level:
+		1:
+			hp = 1
+			speed = 100
+			damage = 5
+			knock_amount = 100
+			attack_size = 1.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	
+	position += angle * speed * delta
+	"""
 	const RANGE = 150
 	# bubble decelerates 
 	if speed > 0:
@@ -15,12 +37,20 @@ func _physics_process(delta):
 	# bubble shoots towards enemy but with random offset for spray effect
 	var direction = Vector2.RIGHT.rotated(rotation+offset)
 	position += direction * speed * delta
-	travel_distance += speed * delta
+
 	# despawn bubble when it slows down enough or if it travels far enough
-	if travel_distance > RANGE or speed <= 5:
+	if speed <= 5:
+		queue_free()
+	"""
+func enemy_hit(charge = 1):
+	hp -= charge
+	if hp <= 0:
 		queue_free()
 
 func _on_body_entered(body):
 	queue_free() # destroy bullet when it collides
 	if body.has_method("take_damage"):
 		body.take_damage(5)
+
+func _on_timer_timeout():
+	queue_free()
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -6,6 +6,31 @@ extends CharacterBody2D
 var health = 100.0
 const SPEED = 50.0
 
+# Attacks
+var bubble = preload("res://scenes/player/attacks/bubble.tscn")
+
+# AttackNodes
+@onready var bubble_timer = get_node("%BubbleTimer")
+@onready var bubble_attack_timer = get_node("%BubbleAttackTimer")
+
+# Bubble
+var bubble_ammo = 0
+var bubble_baseammo = 1
+var bubble_attackspeed = 1.5
+var bubble_level = 1
+
+# Enemy Related (track closest enemy)
+var enemy_Close = []
+
+func _ready():
+	attack()
+
+func attack():
+	if bubble_level > 0:
+		bubble_timer.wait_time = bubble_attackspeed
+		if bubble_timer.is_stopped():
+			bubble_timer.start()
+
 func _physics_process(delta):
 	
 	# TODO: replace HP label with HP bar
@@ -52,3 +77,27 @@ func _physics_process(delta):
 func _on_hurt_box_hurt(damage):
 	health -= damage
 	 # Replace with function body.
+
+
+func _on_bubble_timer_timeout():
+	# load ammo
+	bubble_ammo += bubble_baseammo
+	bubble_attack_timer.start()
+
+
+func _on_bubble_attack_timer_timeout():
+	# shoot ammo
+	if bubble_ammo > 0:
+		var bubble_attack = bubble.instantiate()
+		bubble_attack.position = position
+		bubble_attack.target = get_random_target()
+		bubble_attack.level = bubble_level
+		add_child(bubble_attack)
+		bubble_ammo -= 1
+		if bubble_ammo > 0:
+			bubble_attack_timer.start()
+		else:
+			bubble_attack_timer.stop()
+		
+func get_random_target():
+	pass
